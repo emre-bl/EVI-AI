d745882fdc9b2a6bc90c79ebe904aca1341ba079
mericdemirors <105359055+mericdemirors@users.noreply.github.com>
2023-11-20 21:38:06 +0300

    fixed server-to-client issues
    still has bugs(connections that are already open is a problem)

---
+++ server.py
@@ -0,0 +1,75 @@
+import socket
+import random
+import time
+import pickle
+import cv2
+import json
+
+
+# Function to send image over the network
+def send_image(conn, image, sleep_time):
+    image_data = pickle.dumps(image)
+    done_data = "DONE".encode()
+    data_to_send = image_data + done_data
+
+    conn.send(data_to_send)
+    
+    print("Server: data sent, waiting for client response...")
+    response = conn.recv(1024).decode() # Wait for the client response
+    print("Server: received client response:", response)
+
+    if response == "DONE":
+        print("Server: client processing is done, waiting", sleep_time, "seconds before next image.")
+        time.sleep(sleep_time)
+
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Create a socket
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_address = ('localhost', 12345) # Bind the socket to a specific address and port
+
+while True:
+    try:
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Create a socket
+        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        print("Server: double check the", server_address, "reconnect if any error happened.")
+        server_socket.bind(server_address)
+        break
+    except OSError as e:
+        print(e)
+        if e.errno == 98:  # Address already in use
+            print("Server:", server_address, "already in use. Closing the existing connection.")
+            server_socket.close()
+        else:
+            raise
+
+# Listen for incoming connections
+server_socket.listen(1)
+
+print("Server: listening for connections...")
+
+
+
+video_path = './YOLO_test_videos/video1.webm'
+cap = cv2.VideoCapture(video_path)
+if not cap.isOpened():
+    print("Server: Error: Could not open video file.")
+    exit(0)
+
+
+while True:
+    print("Server: waiting for a connection...")
+    connection, client_address = server_socket.accept()
+    
+    try:
+        print("Server: connection from", client_address)
+        while True:
+            ret, image = cap.read()
+            if not ret:
+                break
+            image = cv2.resize(image, dsize=(224,224))
+            send_image(connection, image, 0.01)
+            
+    finally:
+        print("Server: terminating code.")
+        connection.close()
+        server_socket.close()
