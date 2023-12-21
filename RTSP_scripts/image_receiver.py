04141d443adf7f53ba322cde0ebd61a298d11379
mericdemirors <mericdemirors218@gmail.com>
2023-12-21 23:08:22 +0300

    simplified image sender and receiver codes

--- RTSP_scripts/image_receiver.py
+++ RTSP_scripts/image_receiver.py
@@ -2,38 +2,17 @@ import socket
 import pickle
 import cv2
 
-# funtion to pass image to vision models
-def send_image_to_models(image):
-    cv2.imshow("g", image)
-    cv2.watKey(1)
-    return "yoloout ve depth out"
-    YOLO_out = yolo_model(image)
-    DEPTH_out = depth_model(image)
-
-    # merge YOLO_out, DEPTH_out
-    return YOLO_out, DEPTH_out
-
-# Function to pass vision models output to LLM API
-def send_model_out_to_LLM(out):
-    return "LLMout"
-    LLM_out = LLM_model(out)
-    return LLM_out
-
 # Function to receive image data
 def get_image_data():
     image_data = b""
     
-    while True: # Loop until the entire data is received
-        if 0 < len(image_data) < 4097:
-            print("receiverSide: receiving image...")
-            
+    while True: # Loop until the entire data is received            
         chunk = receiver_socket.recv(4096)
         if not chunk:
             break
 
         image_data += chunk
         if image_data[-14:] == "SENDERSIDEDONE".encode():
-            print("receiverSide: fully received image.")
             image_data = image_data[:-14]
             break
 
@@ -55,15 +34,10 @@ receiver_socket.connect(server_address)
 try:
     while True:
         image = get_image_data()
-
-        model_output = send_image_to_models(image)
-
-        LLM_out = send_model_out_to_LLM(model_output)
-
-        response = LLM_out + "RECEIVERSIDEDONE"
-
-        receiver_socket.send(response.encode())
-finally:
+        cv2.imshow("receier_image", image)
+        cv2.waitKey(1)
     
-    print("receiverSide: terminating code.")
+        receiver_socket.send("RECEIVERSIDEDONE".encode())
+finally:
+    cv2.destroyAllWindows()
     receiver_socket.close()
