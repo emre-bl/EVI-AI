071577fcf3c1aca2c6fc1868034dafb72ad608c6
Zeynep Meriç Aşık <zeynepmeric2@gmail.com>
2024-03-17 21:40:37 +0300

    app

--- pipeline_scripts/mobile_app/lib/api.dart
+++
@@ -1,7 +0,0 @@
-import 'package:http/http.dart' as http;
-
-
-Future runUser(url) async {
-    http.Response response = await http.get(url);
-    return response.body;
-}
--- pipeline_scripts/mobile_app/lib/main.dart
+++ pipeline_scripts/mobile_app/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-//import 'API.dart';
 import 'package:audioplayers/audioplayers.dart';
 import 'dart:async';
 
@@ -18,6 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
