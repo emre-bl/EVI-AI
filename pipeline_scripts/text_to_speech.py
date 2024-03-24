86cc7bde56a9b4fb097e28b9120e2700bb2b95f3
Zeynep Meriç Aşık <zeynepmeric2@gmail.com>
2024-03-24 17:40:26 +0300

    output tr to eng

--- pipeline_scripts/text_to_speech.py
+++ pipeline_scripts/text_to_speech.py
@@ -2,12 +2,12 @@ from gtts import gTTS
 from playsound import playsound
 import os
 
-def text_to_speech(text, lang='tr'):
+def text_to_speech(text, lang='en'):
     tts = gTTS(text=text, lang=lang, slow=False)
     os.makedirs("pipeline_scripts/mobile_app/assets", exist_ok=True)
     tts.save("pipeline_scripts/mobile_app/assets/LLM_output.mp3")
-    playsound("pipeline_scripts/mobile_app/assets/LLM_output.mp3")
+    #playsound("/mobile_app/assets/LLM_output.mp3")
 
 
-"""user_input = "sağa dönün"
-text_to_speech(user_input, lang='tr')"""
+"""user_input = "Checking for obstacles"
+text_to_speech(user_input, lang='en')"""
